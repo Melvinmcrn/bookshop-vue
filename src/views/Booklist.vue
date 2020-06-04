56b12f9aeaa87a8066939776be4dc92507592079
Melvin <melvin@TH-LW60324-M.local>
2020-06-04 15:54:56 +0700

    create Booklist page

---
+++ src/views/Booklist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="booklist">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import books from '@/assets/books.ts';
+
+export default {
+  name: 'Booklist',
+  components: {
+  },
+  data: {
+      books
+  }
+}
+</script>
