fb886c375387fc766b8a6d19f6b850e6ffe0b1f7
Melvin <melvin@TH-LW60324-M.local>
2020-06-04 15:43:55 +0700

    add books data

---
+++ src/assets/books.ts
@@ -0,0 +1,45 @@
+interface Book {
+    id: string;
+    name: string;
+    price: number;
+}
+
+let books:Book[] = [
+    {
+        id: "1",
+        name: "Book1",
+        price:100,
+    },
+    {
+        id: "2",
+        name: "Book2",
+        price:100,
+    },
+    {
+        id: "3",
+        name: "Book3",
+        price:100,
+    },
+    {
+        id: "4",
+        name: "Book4",
+        price:100,
+    },
+    {
+        id: "5",
+        name: "Book5",
+        price:100,
+    },
+    {
+        id: "6",
+        name: "Book6",
+        price:100,
+    },
+    {
+        id: "7",
+        name: "Book7",
+        price:100,
+    },
+]
+
+export default books;
