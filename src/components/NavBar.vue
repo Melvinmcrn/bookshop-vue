40e86c44f7f17563f7dfb2de02c896884d11cae3
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-06-05 15:17:49 +0700

    add link to booklist and mycart in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,13 +2,13 @@
 <div>
     <nav class="flex items-center justify-between flex-wrap bg-blue-800 p-4 shadow-md fixed w-full" >
         <!-- LEFT SIDE -->
-  <div class="flex items-center flex-shrink-0 text-white">
+  <a href='/booklist'><div class="flex items-center flex-shrink-0 text-white">
     <svg class="fill-current h-8 w-8 mr-2" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 20 20"><path d="M6 4H5a1 1 0 1 1 0-2h11V1a1 1 0 0 0-1-1H4a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V5a1 1 0 0 0-1-1h-7v8l-2-2-2 2V4z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Naidin</span>
-  </div>
+  </div></a>
     <!-- RIGHT SIDE -->
   <div  class="flex items-center flex-shrink-0">
-      <svg class="fill-current h-6 w-6 mr-2 text-blue-200 hover:text-white cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 20 20"><path d="M4 2h16l-3 9H4a1 1 0 1 0 0 2h13v2H4a3 3 0 0 1 0-6h.33L3 5 2 2H0V0h3a1 1 0 0 1 1 1v1zm1 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm10 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>
+      <a href='/mycart' ><svg class="fill-current h-6 w-6 mr-2 text-blue-200 hover:text-white cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 20 20"><path d="M4 2h16l-3 9H4a1 1 0 1 0 0 2h13v2H4a3 3 0 0 1 0-6h.33L3 5 2 2H0V0h3a1 1 0 0 1 1 1v1zm1 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm10 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg></a>
   </div>
 </nav>
 <div class="flex items-center justify-between flex-wrap bg-blue-800 p-4 shadow-md w-full" >
