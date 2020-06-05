7f22ac8a37345dedd658ff9da20d96ccd776fff7
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-06-05 14:28:13 +0700

    update book name for every book

--- src/assets/books.ts
+++ src/assets/books.ts
@@ -12,32 +12,32 @@ const books: Book[] = [
     },
     {
         id: "2",
-        name: "Book2",
+        name: "Harry Potter and the Chamber of Secrets",
         price:100,
     },
     {
         id: "3",
-        name: "Book3",
+        name: "Harry Potter and the Prisoner of Azkaban",
         price:100,
     },
     {
         id: "4",
-        name: "Book4",
+        name: "Harry Potter and the Goblet of Fire",
         price:100,
     },
     {
         id: "5",
-        name: "Book5",
+        name: "Harry Potter and the Order of the Phoenix",
         price:100,
     },
     {
         id: "6",
-        name: "Book6",
+        name: "Harry Potter and the Half-Blood Prince",
         price:100,
     },
     {
         id: "7",
-        name: "Book7",
+        name: "Harry Potter and the Deathly Hallows",
         price:100,
     },
 ]
